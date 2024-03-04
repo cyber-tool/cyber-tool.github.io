15e838ccfa3c68df68c8b39291d3465635b81abf
cyber-tool <162180048+cyber-tool@users.noreply.github.com>
2024-03-04 19:39:38 +0000

    Added Routed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import theme from './theme';
+// import { ThemeProvider } from '@mui/material/styles';
+import MainPage from './MainPage';
+import UnZip from './UnZip';
+import BackgroundRemove from './BackgroundRemove';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+       <Routes>
+         <Route path="/" element={<MainPage />} />
+         <Route path="/unzip" element={<UnZip />} />
+         <Route path="/t2m" element={<UnZip />} />
+         <Route path="/rmb" element={<BackgroundRemove />} />
+       </Routes>
+     </Router>
   );
-}
+ }
 
 export default App;
