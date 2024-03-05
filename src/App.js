9d115d378552f2880ae8c247a271ec16ec67c316
cyber-tool <162180048+cyber-tool@users.noreply.github.com>
2024-03-05 18:50:53 +0000

    Added routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import theme from './theme';
 // import { ThemeProvider } from '@mui/material/styles';
 import MainPage from './MainPage';
-import UnZip from './UnZip';
-import BackgroundRemove from './BackgroundRemove';
+import UnZip from './services/UnZip';
+import BackgroundRemove from './services/BackgroundRemove';
+import TorrentToMagnet from './services/TorrentToMagnet';
+import MagnetToTorrent from './services/MagnetToTorrent';
+import WordToPDF from './services/WordToPDF';
+import PDFToWord from './services/PDFToWord';
+import Base64Encode from './services/Base64Encode';
+import Base64Decode from './services/Base64Decode';
 
 function App() {
   return (
@@ -12,8 +18,13 @@ function App() {
        <Routes>
          <Route path="/" element={<MainPage />} />
          <Route path="/unzip" element={<UnZip />} />
-         <Route path="/t2m" element={<UnZip />} />
+         <Route path="/t2m" element={<TorrentToMagnet />} />
+         <Route path="/m2t" element={<MagnetToTorrent />} />
          <Route path="/rmb" element={<BackgroundRemove />} />
+         <Route path="/w2p" element={<WordToPDF />} />
+         <Route path="/p2w" element={<PDFToWord />} />
+         <Route path="/b64en" element={<Base64Encode />} />
+         <Route path="/b64de" element={<Base64Decode />} />
        </Routes>
      </Router>
   );
