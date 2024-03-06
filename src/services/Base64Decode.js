361c1c59869764639fb08856ff6eb89ec89c7671
cyber-tool <162180048+cyber-tool@users.noreply.github.com>
2024-03-06 10:22:05 +0000

    Added API endpoint

--- src/services/Base64Decode.js
+++ src/services/Base64Decode.js
@@ -11,7 +11,7 @@ function Base64Decode() {
 
   const decodeText = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/b64de', {
+      const response = await fetch('https://' + process.env.REACT_APP_API_DOMAIN + '/b64de', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
