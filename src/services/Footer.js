8b6d88d7eeafcb49071ab602e2c2620283574249
cyber-tool <162180048+cyber-tool@users.noreply.github.com>
2024-03-06 15:39:30 +0000

    Created Footer.js

---
+++ src/services/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, Container, Typography, Toolbar } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+const Footer = () => {
+  return (
+    <>
+        <Toolbar/>
+        <Box
+        component="footer"
+        sx={{
+            py: 3,
+            px: 2,
+            mt: 'auto',
+            backgroundColor: (theme) => theme.palette.secondary.main,
+            color: (theme) => theme.palette.secondary.contrastText,
+        }}
+        >
+        <Container maxWidth="lg">
+            <Typography variant="body2" color="text.secondary" align="center" sx={{ pt: 3, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            Made With <FavoriteIcon sx={{ fontSize: '1rem', mx: 0.5 }} /> By Joseph Rasanjana.
+            </Typography>
+            <Typography variant="body2" color="text.secondary" align="center" sx={{ pt: 3 }}>
+            © {new Date().getFullYear()} Cyber Tool. All rights reserved.
+            </Typography>
+        </Container>
+        </Box>
+    </>
+  );
+};
+
+export default Footer;
