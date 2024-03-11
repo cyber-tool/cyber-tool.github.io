beaa2642ac34d475c8e5fbfe5564c958ef296dd2
Cyber Tool <162180048+cyber-tool@users.noreply.github.com>
2024-03-11 08:11:17 +0000

    Added convert to leet page

---
+++ src/app/toleet/page.tsx
@@ -0,0 +1,115 @@
+'use client'
+import React, { useState, ChangeEvent } from 'react';
+import { Box, Button, Container, Grid, TextField, Typography, Paper, useTheme } from '@mui/material';
+
+function ToLeet() {
+
+  const theme = useTheme();
+
+  const [inputText, setInputText] = useState('');
+  const [leetText, setLeetText] = useState('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const decodeText = async () => {
+    try {
+      const response = await fetch('https://' + process.env.NEXT_PUBLIC_API_DOMAIN + '/toleet', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: inputText }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      setLeetText(data.leet_text);
+    } catch (error) {
+      console.error('Failed to Leet:', error);
+      alert('Failed to Leet text. Please try again.');
+    }
+  };
+
+  return (
+    <Container maxWidth="lg">
+      <Box my={4}>
+        <hr/>
+          <Typography align='center' sx={{ color: 'primary.main' }} variant="h3">Convaert To Leet Text</Typography>
+        <hr/>
+        <br/>
+        <Paper elevation={3} sx={{ p: 4 }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Text to Leet Text"
+                variant="outlined"
+                fullWidth
+                multiline
+                minRows={15}
+                maxRows={15}
+                sx={{
+                  '& .MuiInputBase-input': {
+                    color: theme.palette.secondary.dark,
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: theme.palette.secondary.dark,
+                  },
+                  '& .MuiOutlinedInput-notchedOutline': {
+                    borderColor: theme.palette.secondary.contrastText,
+                  },
+                  '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                    borderColor: theme.palette.secondary.dark,
+                  },
+                }}
+                value={inputText}
+                onChange={handleInputChange}
+                placeholder="Enter encoded text here"
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Leet Text"
+                variant="outlined"
+                fullWidth
+                multiline
+                minRows={15}
+                maxRows={15}
+                sx={{
+                  '& .MuiInputBase-input': {
+                    color: theme.palette.secondary.dark,
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: theme.palette.secondary.dark,
+                  },
+                  '& .MuiOutlinedInput-notchedOutline': {
+                    borderColor: theme.palette.secondary.contrastText,
+                  },
+                  '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                    borderColor: theme.palette.secondary.dark,
+                  },
+                }}
+                value={leetText}
+                InputProps={{
+                  readOnly: true,
+                }}
+                placeholder="Decoded text will appear here"
+              />
+            </Grid>
+          </Grid>
+          <Box textAlign="center" mt={3}>
+            <Button variant="contained" color="secondary" onClick={decodeText} size="large">
+              Convert
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
+    </Container>
+  );
+}
+
+export default ToLeet;
